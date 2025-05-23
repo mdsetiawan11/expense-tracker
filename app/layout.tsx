26cd16ec317b49f161878c2401878f7535437706
M. Dadang Setiawan <m.setiawan@bpk.go.id>
2025-05-23 15:02:41 +0700

    dark mode

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Cabin } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "@/components/ui/toaster";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const cabin = Cabin({
   subsets: ["latin"],
@@ -18,9 +19,16 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body className={`${cabin.className} antialiased`}>
-        {children} <Toaster />
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children} <Toaster />
+        </ThemeProvider>
       </body>
     </html>
   );
--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -34,7 +34,7 @@ export function NavMain({
               className="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
             >
               <PlusCircleIcon />
-              <span>Create</span>
+              <span>Add Transaction</span>
             </SidebarMenuButton>
           </SidebarMenuItem>
         </SidebarMenu>
