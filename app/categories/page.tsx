22c1db5b49406690c69f02b0c067b75b1bb32ea2
M. Dadang Setiawan <m.setiawan@bpk.go.id>
2025-06-03 08:05:38 +0700

    Ubah Tombol Category

--- app/categories/page.tsx
+++ app/categories/page.tsx
@@ -53,7 +53,9 @@ export default function Page() {
       <SidebarInset>
         <SiteHeader />
         <div className="flex flex-1 flex-col p-4">
-          <AddSheet onSuccess={loadData} />
+          <div className="flex flex-row justify-end">
+            <AddSheet onSuccess={loadData} />
+          </div>
 
           <div className="@container/main flex flex-1 flex-col gap-2">
             <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
