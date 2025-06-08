9089894c95133a5b4ba5fea8690e5834df8c2148
M. Dadang Setiawan <m.setiawan@bpk.go.id>
2025-06-08 16:39:36 +0700

    remove add transaction button in sidebar

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -13,6 +13,7 @@ import {
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Separator } from "./ui/separator";
+import { AddSheet } from "@/app/transactions/add-sheet";
 
 export function NavMain({
   items,
@@ -27,17 +28,6 @@ export function NavMain({
   return (
     <SidebarGroup>
       <SidebarGroupContent className="flex flex-col gap-2">
-        <SidebarMenu>
-          <SidebarMenuItem className="flex items-center gap-2">
-            <SidebarMenuButton
-              tooltip="Create"
-              className="min-w-8 bg-primary text-primary-foreground duration-200 ease-linear hover:bg-primary/90 hover:text-primary-foreground active:bg-primary/90 active:text-primary-foreground"
-            >
-              <PlusCircleIcon />
-              <span>Add Transaction</span>
-            </SidebarMenuButton>
-          </SidebarMenuItem>
-        </SidebarMenu>
         <Separator className="my-1" />
         <SidebarMenu>
           {items.map((item) => (
